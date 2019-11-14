9ed7a2c8bd1d878380a093af8fe8a331c71c3ba5
Daniel Gordon <d_gordon1@icloud.com>
2019-11-14 15:38:00 +1100

    starting point for FE livecode.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,28 @@
 Garden.destroy_all if Rails.env.development?
 
-
-Garden.create!(
+epic = Garden.create!(
   name: "Epic hanging stuff garden",
   banner_url: "https://www.historicmysteries.com/wp-content/uploads/2010/05/anging-gardens-of-babylon-e1566297262106-1024x555.png"
 )
-Garden.create!(
+little = Garden.create!(
   name: "My Little Garden",
   banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_1.jpg"
 )
 
-Garden.create!(
-  name: "My Other Garden",
-  banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_2.jpg"
+Plant.create!(
+  name: "Monstera",
+  image_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/plants/monstera.jpg",
+  garden: epic
 )
 
+Plant.create!(
+  name: "Philo",
+  image_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/plants/philo.jpg",
+  garden: epic
+)
 
+Plant.create!(
+  name: "Dieff",
+  image_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/plants/dieffenbachia.jpg",
+  garden: little
+)
